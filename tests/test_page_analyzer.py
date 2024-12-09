13f828debb99d37f52f2e61542553f99ea154ea0
AlloKuz <a.o.kuzmin.rus@gmail.com>
2024-12-09 14:58:38 +0300

    fix

--- tests/test_page_analyzer.py
+++ tests/test_page_analyzer.py
@@ -27,8 +27,3 @@ def runner(app):
 def test_index(client):
     response = client.get("/")
     assert "Бесплатно проверяйте сайты на SEO-пригодность" in response.text
-
-
-def test_urls(client):
-    response = client.get("/urls")
-    assert "Последняя проверка" in response.text
