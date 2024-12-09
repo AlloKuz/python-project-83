ff1a97e2a8f07d8e3b2a2cbce1cf5c8b0ae6cac4
AlloKuz <a.o.kuzmin.rus@gmail.com>
2024-12-09 16:39:57 +0300

    fix

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
-from page_analyzer.utils import url_validate, url_normalize
+from page_analyzer.utils import url_validate, url_normalize # type: ignore
 
-import pytest
+import pytest # type: ignore
 
 
 @pytest.mark.parametrize("url, answer", [
